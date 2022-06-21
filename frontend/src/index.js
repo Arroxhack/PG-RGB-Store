ee823821da674591175af323f507ace2bbff9355
gioantonucci <gioantonucci@hotmail.com>
2022-06-21 00:21:56 -0300

    app en index!

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,19 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
-import DetailProduct from './components/detailProduct';
-import { Provider } from 'react-redux';
-import store from './redux/store'
 
 ReactDOM.render(
-  <Provider store={store}>
   <React.StrictMode>
-    <BrowserRouter>
-    <DetailProduct />
-    </BrowserRouter>
-  </React.StrictMode>
-  </Provider>,
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
