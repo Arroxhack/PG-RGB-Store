b6bc0468e321af2e919d8dc44e425b687c586aae
Your Name <julian.sonido@gmail.com>
2022-06-21 20:39:34 -0300

    Probando

---
+++ frontend/src/components/LogIn/LogIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function LogIn() {
+  return (
+    <div className="container">
+        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">
+            Find Friends
+        </button>
+    </div>
+  )
+}
