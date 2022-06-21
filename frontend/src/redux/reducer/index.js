c4a184456057df4aaf3ccc8b7f7a778ddc86690b
NachooNazar <94497872+NachooNazar@users.noreply.github.com>
2022-06-20 22:23:54 -0300

    files/reducer-types-actions : create product detail

--- frontend/src/redux/reducer/index.js
+++ frontend/src/redux/reducer/index.js
@@ -1,20 +1,25 @@
-import {GET_ALL_COMPONENTS} from "../types/index";
+import { GET_ALL_COMPONENTS, GET_PRODUCT_DETAIL } from '../types/index';
 
 const initialState = {
-    allComponents:[],
-  };
-  
-  const reducer = (state = initialState, action) => {
-    switch (action.type) {
-      
-      case GET_ALL_COMPONENTS:
-        return{
-          ...state,
-          allComponents: action.payload
-        }
-        default:
-        return state;
-    }
-  };
-  
-  export default reducer;
+  allComponents: [],
+  detail: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ALL_COMPONENTS:
+      return {
+        ...state,
+        allComponents: action.payload,
+      };
+    case GET_PRODUCT_DETAIL:
+      return {
+        ...state,
+        detail: action.payload,
+      };
+    default:
+      return { ...state };
+  }
+};
+
+export default reducer;
