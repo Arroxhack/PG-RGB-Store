5ae6c46858be90f300918bb768fa0f2ae6e2b36e
gioantonucci <gioantonucci@hotmail.com>
2022-06-21 00:14:44 -0300

    detailProduct.jsx

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
+import DetailProduct from './components/detailProduct';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+    <DetailProduct />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
