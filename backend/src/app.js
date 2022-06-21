caef9ed897b6920b9b16276cd0f4ec1c84a880f7
tinorusso1012 <valentinorusso2002@gmail.com>
2022-06-21 18:26:44 -0300

    Autenticacion

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,10 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const session = require("express-session");
+const passport = require("passport");
+const Strategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
 
 require('./db.js');
 
@@ -36,4 +40,84 @@ server.use((err, req, res, next) => {
   res.status(status).send(message);
 });
 
+// ---------- AUTENTICACION ------------ // 
+passport.use(
+  new Strategy(function (username, password, done) {
+    console.log('paso uno de la autenticación')
+    findUser({userName: username}) //busca en mongoDB el usuario
+      .then((user) => {
+        if (!user) {
+          return done(null, false);
+        }        
+        if (user) {
+          // Voy a hacer la comparación y evaluar el resultado
+          bcrypt.compare(password, user.password )          
+          .then((res) => {
+            // console.log(res, 'la respuesta de la promesa')
+            if(res === false) { // No hay coincidencia entre las password
+              return done(null, false);
+            }
+            if(res === true) { // Si hay coincidencia entre las password
+              // console.log(user, res, ' user en la 54');
+              return done(null, user);
+            }
+          })        
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        return done(err);
+      });
+  })
+);
+
+
+passport.serializeUser(function(user, done) {
+  console.log('paso dos de la autenticación')
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(_id, done) {  
+  console.log('paso tres de la autenticación')
+  findUser({_id: _id})
+  .then((user) => {
+      done(null, user);
+    })    
+  .catch(err => {
+      return done(err);
+    })
+});
+
+server.use(
+  session({
+    secret: SECRET || 'secret',
+    resave: true,
+    saveUninitialized: true,
+  })
+); // estaban seteados a false
+
+server.use(passport.initialize());
+server.use(passport.session());
+
+// Middleware para mostrar la sesión actual en cada request
+server.use((req, res, next) => {
+  // console.log(req.session, ' esto es req.session 120');
+  // console.log(req.user, ' esto es req.user 121');
+  next();
+});
+server.get('/login',  (req, res) => {
+    res.send('Username o contraseña incorrecta');
+});
+
+
+server.use("/", routes);
+
+// --- --- Error catching endware.
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 module.exports = server;
