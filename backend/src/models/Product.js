9f249aed3d42cd057542178f478280f10f3d7f94
NachooNazar <94497872+NachooNazar@users.noreply.github.com>
2022-06-21 15:32:59 -0300

    feat/routes/Product : ruta search by name

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,9 +1,9 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("product", {
+  sequelize.define('product', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
