abece951391f27cfa59eb9320c88697d1c1c9605
tinorusso1012 <valentinorusso2002@gmail.com>
2022-06-21 21:26:55 -0300

    Actualizacion de App and Controllers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const routes = require('./routes/index.js');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const routes = require("./routes/index.js");
 const session = require("express-session");
 const passport = require("passport");
 const Strategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
-
-require('./db.js');
+const { findUser } = require("./controllers/users")
+require("./db.js");
 
 const server = express();
 
-server.name = 'API';
+server.name = "API";
 
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
-server.use('/', routes);
+server.use("/", routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => {
@@ -40,28 +40,29 @@ server.use((err, req, res, next) => {
   res.status(status).send(message);
 });
 
-// ---------- AUTENTICACION ------------ // 
+// ---------- AUTENTICACION ------------ //
 passport.use(
   new Strategy(function (username, password, done) {
-    console.log('paso uno de la autenticación')
-    findUser({userName: username}) //busca en mongoDB el usuario
+    console.log("paso uno de la autenticación");
+    findUser(username) //busca en Postgress el usuario
       .then((user) => {
         if (!user) {
           return done(null, false);
-        }        
+        }
         if (user) {
           // Voy a hacer la comparación y evaluar el resultado
-          bcrypt.compare(password, user.password )          
-          .then((res) => {
+          bcrypt.compare(password, user.password).then((res) => {
             // console.log(res, 'la respuesta de la promesa')
-            if(res === false) { // No hay coincidencia entre las password
+            if (res === false) {
+              // No hay coincidencia entre las password
               return done(null, false);
             }
-            if(res === true) { // Si hay coincidencia entre las password
+            if (res === true) {
+              // Si hay coincidencia entre las password
               // console.log(user, res, ' user en la 54');
               return done(null, user);
             }
-          })        
+          });
         }
       })
       .catch((err) => {
@@ -71,26 +72,25 @@ passport.use(
   })
 );
 
-
-passport.serializeUser(function(user, done) {
-  console.log('paso dos de la autenticación')
+passport.serializeUser(function (user, done) {
+  console.log("paso dos de la autenticación");
   done(null, user._id);
 });
 
-passport.deserializeUser(function(_id, done) {  
-  console.log('paso tres de la autenticación')
-  findUser({_id: _id})
-  .then((user) => {
+passport.deserializeUser(function (_id, done) {
+  console.log("paso tres de la autenticación");
+  findUser({ _id: _id })
+    .then((user) => {
       done(null, user);
-    })    
-  .catch(err => {
-      return done(err);
     })
+    .catch((err) => {
+      return done(err);
+    });
 });
 
 server.use(
   session({
-    secret: SECRET || 'secret',
+    secret: SECRET || "secret",
     resave: true,
     saveUninitialized: true,
   })
@@ -105,10 +105,9 @@ server.use((req, res, next) => {
   // console.log(req.user, ' esto es req.user 121');
   next();
 });
-server.get('/login',  (req, res) => {
-    res.send('Username o contraseña incorrecta');
+server.get("/login", (req, res) => {
+  res.send("Username o contraseña incorrecta");
 });
-
 
 server.use("/", routes);
 
--- backend/src/db.js
+++ backend/src/db.js
@@ -41,7 +41,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 
-const { Product, Category } = sequelize.models;
+const { Product, Category , User } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
