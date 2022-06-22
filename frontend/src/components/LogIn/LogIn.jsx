36993f053466dce2d71c6f3e8e66a80356c4fc78
Your Name <julian.sonido@gmail.com>
2022-06-22 10:22:14 -0300

    Ruteo del logIn

--- frontend/src/components/LogIn/LogIn.jsx
+++ frontend/src/components/LogIn/LogIn.jsx
@@ -4,7 +4,7 @@ export default function LogIn() {
   return (
     <div className="container">
         <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">
-            Find Friends
+            LogIn
         </button>
     </div>
   )
