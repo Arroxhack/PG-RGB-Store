eeb8e908abe7fd635618d8e55fdba76c1babf668
Your Name <julian.sonido@gmail.com>
2022-06-22 14:20:55 -0300

    LogIn hecho

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 
+
 function NavBar({ toggleOpen }) {
+  const username = localStorage.getItem("username")
+  console.log(username);
+
   return (
     <nav className="flex justify-evenly items-center h-36 bg-gray-300 text-black relative shadow-sm bg-primary-200">
       <Link to="/" className="flex flex-col items-center text-primary-400 font-Open text-5xl font-extrabold">
@@ -29,9 +33,10 @@ function NavBar({ toggleOpen }) {
       </div>
      
       <div className="flex gap-5">
+        {username ? <h2>Bienvenido {username}</h2> : 
         <Link to="/logIn" className="bg-primary-400 font-Open px-5 py-1 rounded-lg text-primary-200 uppercase font-semibold hover:bg-primary-300">
-          Ingresar
-        </Link>
+        Ingresar
+      </Link>}
         <Link to="" className="bg-primary-400 font-Open px-5 py-1 rounded-lg text-primary-200 uppercase font-semibold hover:bg-primary-300">
           Carrito
         </Link>
