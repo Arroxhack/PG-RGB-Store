1cccd5e47c2b31e55ad90682a874eb1c248e8379
gioantonucci <gioantonucci@hotmail.com>
2022-06-22 12:03:58 -0300

    avances tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import { Route, Routes } from "react-router";
 import ContainerProduct from "./components/ContainerProduct/ContainerProduct";
 import Home from "./components/Home/Home.jsx";
-import DetailProduct from "./components/DetailProduct/DetailProduct.jsx";
+import DetailProduct from "./components/DetailProduct/DetailProduct";
 import Cart from "./components/Cart/Cart";
+
 function App() {
   return (
     <div className='Font-Open'>
       <Routes>
-        <Route path="/" element={<ContainerProduct />} />
+        <Route path="/" element={<DetailProduct />} />
         <Route path="/home" element={<Home />} exact />
         <Route path="/products/:id" element={<DetailProduct />} exact />
         <Route path="/cart" element={<Cart />} />
