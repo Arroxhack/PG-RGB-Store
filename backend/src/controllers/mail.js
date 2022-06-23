a6c14ccaae8b7492b13c0eb21ee7682a7ccb7155
NachooNazar <94497872+NachooNazar@users.noreply.github.com>
2022-06-23 01:53:25 -0300

    asd

---
+++ backend/src/controllers/mail.js
@@ -0,0 +1,11 @@
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: testAccount.user, // generated ethereal user
+    pass: testAccount.pass, // generated ethereal password
+  },
+});
