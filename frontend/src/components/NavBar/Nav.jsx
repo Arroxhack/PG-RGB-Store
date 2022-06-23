f72a95ecdc510af06dedf324688a4ea8198ce06c
gioantonucci <gioantonucci@hotmail.com>
2022-06-23 18:10:46 -0300

    arregle cositas

--- frontend/src/components/NavBar/Nav.jsx
+++ frontend/src/components/NavBar/Nav.jsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <div className='flex flex-row justify-evenly bg-primary h-12 items-center font-semibold text-xl uppercase'>
-        <Link to='/categorias' className='hover:bg-primary-300 hover:py-3 px-4 '>
+        <Link to='/categories' className='hover:bg-primary-300 hover:py-3 px-4 '>
         <p>Categorías</p>
         </Link>
-        <Link to='/categorias/notebooks' className='hover:bg-primary-300 hover:py-3 px-4'>
+        <Link to='/categories/notebooks' className='hover:bg-primary-300 hover:py-3 px-4'>
         Notebooks
         </Link>
-        <Link to='/categorias/pc-armadas' className='hover:bg-primary-300 hover:py-3 px-4'>
+        <Link to='/categories/pc-armadas' className='hover:bg-primary-300 hover:py-3 px-4'>
         PC Armadas
         </Link>
-        <Link to='/categorias/placas-de-videos' className='hover:bg-primary-300 hover:py-3 px-4'>
-        Placas de video
+        <Link to='/categories/placas-de-videos' className='hover:bg-primary-300 hover:py-3 px-4'>
+        Placas de videos
         </Link>
         <Link to='/arma-tu-pc' className='hover:bg-primary-300 hover:py-3 px-4'>
         <p>Armá tu pc</p>
