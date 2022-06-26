cb1a0baf64aa6c3851c59eb40e7356d5adc24a63
Your Name <julian.sonido@gmail.com>
2022-06-26 18:06:11 -0300

    Empezando con paypal

--- frontend/src/components/Paypal/Checkout.jsx
+++ frontend/src/components/Paypal/Checkout.jsx
@@ -6,35 +6,70 @@ export default function Checkout() {
 
     const paypal = useRef();
 
+    
     useEffect(() => {
         window.paypal.Buttons({
-            createOrder: (data, actions, err) => { //funcion que crea ordenes
-                return actions.order.create({
-                    intent: "CAPTURE",
-                    purchase_units: [
-                        {
-                            description: "Cool looking table",
-                            amout: {
-                                currency_code:"USD", 
-                                value: 650.00
-                            }
+            // Sets up the transaction when a payment button is clicked
+            createOrder: (data, actions) => {
+              return actions.order.create({
+                intent: "CAPTURE",
+                purchase_units: [{
+                  amount: {
+                    currency_code: "USD",
+                    value: '2', // Can also reference a variable or function
+                    breakdown: {
+                        item_total: { // Required when including the "items" array !!!!
+                            currency_code: "USD",
+                            value: '2'
                         }
-                    ]
-                })
+                    }
+                  },
+                //   description: "teclado de pc"
+                  items: [
+                    {
+                        name: "Item1", /* Shows within upper-right dropdown during payment approval */
+                        description: "Item1 genial", /* Item details will also be in the completed paypal.com transaction view */
+                        unit_amount: {
+                        currency_code: "USD",
+                        value: "1"
+                        },
+                        quantity: "1"
+                    },
+                    {
+                        name: "Item2", /* Shows within upper-right dropdown during payment approval */
+                        description: "Item2 muy bueno", /* Item details will also be in the completed paypal.com transaction view */
+                        unit_amount: {
+                            currency_code: "USD",
+                            value: "1"
+                        },
+                        quantity: "1"
+                    },
+                  ]
+                }],
+              });
             },
-            onApprove: async (data, actions) => { // si se aprueba la compra 
-                const order = await actions.order.capture()
-                console.log(order) 
+            // Finalize the transaction after payer approval
+            onApprove: (data, actions) => {
+              return actions.order.capture().then(function(orderData) {
+                // Successful capture! For dev/demo purposes:
+                console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                const transaction = orderData.purchase_units[0].payments.captures[0];
+                alert(`Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`);
+                // When ready to go live, remove the alert and show a success message within this page. For example:
+                // const element = document.getElementById('paypal-button-container');
+                // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+                // Or go to another URL:  actions.redirect('thank_you.html');
+              });
             },
-            onError: (err) => { // si no se aprueba
+            onError: (err) => {
                 console.log(err)
             }
         }).render(paypal.current)
     }, [])
 
   return (
-    <div>
-        <div ref = {paypal}></div>
+    <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "500px"}}>
+        <div style={{width: 200}} ref = {paypal}></div>
     </div>
   )
 }
