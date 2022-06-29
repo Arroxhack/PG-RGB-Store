e931dbb328ad7bae114cf55226395d6537394b3a
gioantonucci <gioantonucci@hotmail.com>
2022-06-29 15:40:15 -0300

    ahi va otro

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,7 +23,6 @@ root.render(
     </Provider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
