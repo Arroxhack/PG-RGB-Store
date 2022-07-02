d85a5edd477ead40ca5150f546347274ac5c042a
tinorusso1012 <valentinorusso2002@gmail.com>
2022-07-02 16:35:53 -0300

    CommentReview

--- frontend/src/components/Paypal/crearComentario.js
+++ frontend/src/components/Paypal/crearComentario.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
-export async function  CrearComentarioReview(idUser,idProduct,idCompra){
-    const result =  await axios({
-        method: "post",
-        url: "http://localhost:3001/NewReview",
-        data: {idUser,idProduct,idCompra }, // email
-        headers: { "X-Requested-With": "XMLHttpRequest" },
-        withCredentials: true,
-      });
-      if (result[0] === "E" && result[1] === "r" && result[2] === "r"){
-        return "Error imprevisto";
-      } 
-      return  "Done";
+export async function CrearComentarioReview(Username, ArrayProduct, idCompra) {
+  for (let i = 0; i < ArrayProduct.length; i++) {
+    const idProduct = Number(ArrayProduct[i]);
+    await axios({
+      method: "post",
+      url: "http://localhost:3001/NewReview",
+      data: { Username, idProduct, idCompra }, // email
+      headers: { "X-Requested-With": "XMLHttpRequest" },
+      withCredentials: true,
+    });
+  }
 }
