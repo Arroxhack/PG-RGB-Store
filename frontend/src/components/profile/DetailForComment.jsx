35cd4c36b9795fcf3656cc87ccbc2517ccc688cc
unknown <davidbarriosok25@gmail.com>
2022-07-04 12:15:13 -0300

    Deploy

--- frontend/src/components/profile/DetailForComment.jsx
+++ frontend/src/components/profile/DetailForComment.jsx
@@ -203,7 +203,7 @@ function DetailProduct() {
                 onChange={(e) => setComentario(e.target.value)}
                 type="text"
                 placeholder="Write here your commend"
-                class="px-3 p-10 py-4 placeholder-slate-300 text-3xl text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                class="px-3 p-10 py-4 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full"
               />
             </div>
             <button
