3d70a0f1aa46f06b9073779d5aaacd9df275c2f4
gioantonucci <gioantonucci@hotmail.com>
2022-07-08 16:04:52 -0300

    falta el carrito

--- frontend/src/components/BuildPc/Armado.jsx
+++ frontend/src/components/BuildPc/Armado.jsx
@@ -6,21 +6,24 @@ import NavBar from "../NavBar/NavBar";
 import Selects from "./Selects";
 
 function Armado() {
-  const [selectedCPU, setSelectedCPU] = useState();
-  const [selectedMother, setSelectedMother] = useState();
-  const [selectedRam, setSelectedRam] = useState();
-  const [selectedGPU, setSelectedGPU] = useState();
-  const [selectedHDD, setSelectedHDD] = useState();
-  const [selectedSSD, setSelectedSSD] = useState();
-  const [selectedSSDM2, setSelectedSSDM2] = useState();
-  const [selectedPSUS, setSelectedPSUS] = useState();
-  const [selectedCase, setSelectedCase] = useState();
+  const [selectedCPU, setSelectedCPU] = useState([]);
+  const [selectedMother, setSelectedMother] = useState([]);
+  const [selectedRam, setSelectedRam] = useState([]);
+  const [selectedGPU, setSelectedGPU] = useState([]);
+  const [selectedHDD, setSelectedHDD] = useState([]);
+  const [selectedSSD, setSelectedSSD] = useState([]);
+  const [selectedSSDM2, setSelectedSSDM2] = useState([]);
+  const [selectedPSUS, setSelectedPSUS] = useState([]);
+  const [selectedCase, setSelectedCase] = useState([]);
+
+  const [build,setBuild]=useState({});
+
   const { addProductToCart } = useContext(CartContext);
   //{CPU:{},Motherboard:{},GPU:{},Ram:{},'Power Supply':{},Case:{},SSD:{},HDD:{},'SSD M.2':{}}
   //algo asi se tendria que ver el build
   const sendCard = (e) => {
     e.preventDefault();
-    addProductToCart();
+    addProductToCart(build);
   };
   const dispatch = useDispatch();
 
@@ -29,88 +32,162 @@ function Armado() {
   }, [dispatch]);
 
   const allProducts = useSelector((state) => state.allProducts);
-  console.log(selectedCPU)
+
   return (
     <div className="bg-primary-100">
       <NavBar />
-      <div>
-        {/* <Brands /> */}
+      <div>Your pc:</div>
+      <div className="">
+   
         <div>
-          Your pc:
-          <br />
+          <Selects
+            name="CPU"
+            handleChange={(e) => {
+              let valueArray = e.target.value;
+              setSelectedCPU(JSON.parse(valueArray));
+            }}
+          />
+
+          <div>{selectedCPU.name}</div>
+          <img
+            alt="image not found"
+            width={100}
+            src={selectedCPU.image ? selectedCPU.image[0] : "https://cdn.pixabay.com/photo/2017/07/09/20/48/cpu-2488091_960_720.png"}
+          />
         </div>
-        <Selects
-          name="CPU"
-          handleChange={(e) => {
-            setSelectedCPU(e.target.value);
-          }}
-        />
-        <div> {selectedCPU} </div>
-        <img
-          alt="image not found"
-          src={allProducts.map((p) =>
-            p === { selectedCPU } ? p.image[0] : null
-          )}
-        />
-        <br />
-        <Selects
-          name="Motherboard"
-          handleChange={(e) => {
-            setSelectedMother(e.target.value);
-          }}
-        />
-        {selectedMother} <br />
-        <Selects
-          name="Ram"
-          handleChange={(e) => {
-            setSelectedRam(e.target.value);
-          }}
-        />
-        {selectedRam} <br />
-        <Selects
-          name="GPU"
-          handleChange={(e) => {
-            setSelectedGPU(e.target.value);
-          }}
-        />
-        {selectedGPU} <br />
-        <Selects
-          name="HDD"
-          handleChange={(e) => {
-            setSelectedHDD(e.target.value);
-          }}
-        />
-        {selectedHDD} <br />
-        <Selects
-          name="SSD"
-          handleChange={(e) => {
-            setSelectedSSD(e.target.value);
-          }}
-        />
-        {selectedSSD} <br />
-        <Selects
-          name="SSD M.2"
-          handleChange={(e) => {
-            setSelectedSSDM2(e.target.value);
-          }}
-        />
-        {selectedSSDM2} <br />
-        <Selects
-          name="Power Supply"
-          handleChange={(e) => {
-            setSelectedPSUS(e.target.value);
-          }}
-        />
-        {selectedPSUS}
-        <br />
-        <Selects
-          name="Case"
-          handleChange={(e) => {
-            setSelectedCase(e.target.value);
-          }}
-        />
-        {selectedCase}
-        <br />
+        <div>
+          <Selects
+            name="Motherboard"
+            handleChange={(e) => {
+              let valueArray = e.target.value;
+              setSelectedMother(JSON.parse(valueArray));
+            
+            }}
+          />
+
+          <div>{selectedMother.name}</div>
+          <img
+            alt="image not found"
+            width={100}
+            src={selectedMother.image ? selectedMother.image[0] : "https://kawaiistore.com.sv/wp-content/uploads/2020/05/1-41-512.png" }
+          />
+        </div>
+        <div>
+          <Selects
+            name="Ram"
+            handleChange={(e) => {
+              let valueArray = e.target.value;
+              setSelectedRam(JSON.parse(valueArray));
+            }}
+          />
+          <div>{selectedRam.name}</div>
+          <img
+            alt="image not found"
+            width={100}
+            src={selectedRam.image ? selectedRam.image[0] : "https://d30y9cdsu7xlg0.cloudfront.net/png/32078-200.png"}
+          />
+        </div>
+
+        <div>
+          <Selects
+            name="GPU"
+            handleChange={(e) => {
+              let valueArray = e.target.value;
+              setSelectedGPU(JSON.parse(valueArray));
+            }}
+          />
+
+          <div>{selectedGPU.name}</div>
+          <img
+            alt="image not found"
+            width={100}
+            src={selectedGPU.image ? selectedGPU.image[0] : "https://static.thenounproject.com/png/636353-200.png"}
+          />
+        </div>
+
+        <div>
+          <Selects
+            name="HDD"
+            handleChange={(e) => {
+              let valueArray = e.target.value;
+              setSelectedHDD(JSON.parse(valueArray));
+            }}
+          />
+
+          <div>{selectedHDD.name}</div>
+          <img
+            alt="image not found"
+            width={100}
+            src={selectedHDD.image ? selectedHDD.image[0] : "https://www.pngrepo.com/download/173555/hard-drive.png"}
+          />
+        </div>
+
+        <div>
+          <Selects
+            name="SSD"
+            handleChange={(e) => {
+              let valueArray = e.target.value;
+              setSelectedSSD(JSON.parse(valueArray));
+            }}
+          />
+
+          <div>{selectedSSD.name}</div>
+          <img
+            alt="image not found"
+            width={100}
+            src={selectedSSD.image ? selectedSSD.image[0] : "https://cdn-icons-png.flaticon.com/512/64/64199.png"}
+          />
+        </div>
+
+        <div>
+          <Selects
+            name="SSD M.2"
+            handleChange={(e) => {
+              let valueArray = e.target.value;
+              setSelectedSSDM2(JSON.parse(valueArray));
+            }}
+          />
+
+          <div>{selectedSSDM2.name}</div>
+          <img
+            alt="image not found"
+            width={100}
+            src={selectedSSDM2.image ? selectedSSDM2.image[0] : "https://cdn1.iconfinder.com/data/icons/hardware-gliph/48/hardware-computer-electronic-ssd-m2-drive-storage-data-512.png"}
+          />
+        </div>
+
+        <div>
+          <Selects
+            name="Power Supply"
+            handleChange={(e) => {
+              let valueArray = e.target.value;
+              setSelectedPSUS(JSON.parse(valueArray));
+            }}
+          />
+
+          <div>{selectedPSUS.name}</div>
+          <img
+            alt="image not found"
+            width={100}
+            src={selectedPSUS.image ? selectedPSUS.image[0] : "https://cdn-icons-png.flaticon.com/512/1470/1470137.png"}
+          />
+        </div>
+
+        <div>
+          <Selects
+            name="Case"
+            handleChange={(e) => {
+              let valueArray = e.target.value;
+              setSelectedCase(JSON.parse(valueArray));
+            }}
+          />
+          <div>{selectedCase.name}</div>
+          <img
+            alt="image not found"
+            width={100}
+            src={selectedCase.image ? selectedCase.image[0] : "https://static.thenounproject.com/png/403047-200.png"}
+          />
+        </div>
       </div>
       <button
         onClick={sendCard}
--- frontend/src/components/BuildPc/Selects.jsx
+++ frontend/src/components/BuildPc/Selects.jsx
@@ -9,18 +9,28 @@ function Selects({ name, handleChange }) {
 
   useEffect(() => {
     dispatch(getAllProducts());
-   
   }, [dispatch]);
 
   const allProducts = useSelector((state) => state.allProducts);
-  console.log(allProducts)
+  //console.log(allProducts);
 
   return (
     <>
-      <label className="p-2 mx-0 mb-2 mt-0 block font-Open font-bold leading-none cursor-pointer" htmlFor={id}>{label}</label>
-      <select className="border-solid rounded p-2 mx-0 mb-2 mt-0 block text-base leading-none bg-secundary-250 cursor-pointer" name={id} id={id} onChange={handleChange}>
-      <option value="">Choose your {name}</option>
-      {allProducts.map((p) => (p.category[0] === `${name}` ? <option  value={p.image}> {p.name} </option>: null))}
+      <label
+        className="p-2 mx-0 mb-2 mt-0 block font-Open font-bold leading-none cursor-pointer"
+        htmlFor={id}
+      >
+        {label}
+      </label>
+      <select
+        className="border-solid rounded p-2 mx-0 mb-2 mt-0 block text-base leading-none bg-secundary-250 cursor-pointer"
+        name={id}
+        id={id}
+        onChange={handleChange}
+      >
+        <option value="">Choose your {name}</option>
+        {allProducts.map((p) => p.category[0] === `${name}` ? ( <option id={p.id} value={JSON.stringify(p)}> {p.name} </option>) : null
+        )}
       </select>
     </>
   );
