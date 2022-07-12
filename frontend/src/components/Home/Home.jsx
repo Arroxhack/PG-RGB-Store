e05ec4d880e02eb1a316b78fbde0d22867ff9f01
NachooNazar <94497872+NachooNazar@users.noreply.github.com>
2022-07-12 16:57:57 -0300

    Arreglado

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -88,7 +88,7 @@ function Home() {
       <div className="w-full">
         <button  className="fixed bottom-2  sm:w-9  lg:h-12 lg:w-full md:w-full  flex items-center justify-end  rounded-2xl
          gap-1  sm:right-2">
-        <p className="text-xl text-primary-400 font-Open sm:hidden lg:block md:block w-26">Chat with us</p>
+       
         <a id="whatsapp" title="Whatsapp" href="https://wa.me/543434720830?text=" target="_blank" >
         <p className="text-xl text-primary-400 font-Open sm:hidden lg:block md:block">Chat with us</p>
         < SiWhatsapp className="h-7 w-9 relative text-primary-400 "/>
